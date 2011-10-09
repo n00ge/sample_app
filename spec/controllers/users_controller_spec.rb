39647a81aa64bad0a829531972dc5ef3e5ce3d12
Chad Calhoun <chad.m.calhoun@gmail.com>
2011-10-09 15:22:14 -0400

    Added a User factory and tests for the show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,14 +7,32 @@ render_views
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+  describe "GET 'show'" do
+      
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should be successful" do
+      get :show, :id => @user.id
+      response.should be_success
+    end
+
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+
+  end
+
   describe "GET 'new'" do
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
 
     it "should have the right title" do
-    	get 'new'
+    	get :new
     	response.should have_selector('title', :content => "Sign up")
     end
   end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+	user.name						"Chad Calhoun"
+	user.email						"chad@marketart.com"
+	user.password					"foobar"
+	user.password_confirmation  	"foobar"
+end
