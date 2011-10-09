fcdc512d49573b7b896f9b0c432bab563ba527be
Chad Calhoun <chad.m.calhoun@gmail.com>
2011-10-09 14:14:19 -0400

    Added password and encrypted password attributes to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,29 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	attr_accessor	:password
+	attr_accessible :name, :email, :password, :password_confirmation
 
 	email_regex = /\A[\w.+\-]+@[a-zA-Z\-\d.]+\.[a-zA-Z]{1,6}\z/
 
 	validates :name, 	:presence 			=> true, 
 					  	:length 				=> { :maximum => 50 }
+
 	validates :email, 	:presence			=> true,
 						:format 			=> { :with 	=> email_regex },
 						:uniqueness 		=> { :case_sensitive => false }
+
+ 	validates :password, 	:presence			=> true,
+ 							:confirmation		=> true,
+ 							:length				=> { :within => 6..40 }
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111009145646) do
+ActiveRecord::Schema.define(:version => 20111009181021) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
