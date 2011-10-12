46f783a54a6a065f0ed909ba3ec7b76a58e55e69
Chad Calhoun <chad.m.calhoun@gmail.com>
2011-10-11 20:56:58 -0400

    added some sessions tests and controller updates to submit the signin page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,27 @@
 class SessionsController < ApplicationController
+
   def new
   	@title = 'Sign in'
   end
 
+  def create
+  	user = User.authenticate( params[:session][:email], params[:session][:password] )
+
+  	if user.nil?
+  		flash.now[:error] = 'Invalid email/password combination.'
+  		@title = 'Sign in'
+  		render :new
+  	else
+  		# handle successful signin
+  	end
+
+  end
+
+  def destroy
+  	# @title = 
+  end
+
+  class << self
+
+  end
 end
