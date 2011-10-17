450eec31a62d29cefa153d679c733d57f6b69103
Chad Calhoun <chad.m.calhoun@gmail.com>
2011-10-16 22:13:54 -0400

    User relationships following/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,63 +1,89 @@
 class User < ActiveRecord::Base
-	attr_accessor	:password
-	attr_accessible :name, :email, :password, :password_confirmation
-	
-	has_many :microposts, :dependent => :destroy
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+  
+  has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id", 
+                                    :dependent => :destroy
 
-	email_regex = /\A[\w.+\-]+@[a-zA-Z\-\d.]+\.[a-zA-Z]{1,6}\z/
+  has_many :following, :through => :relationships, 
+                                    :source => :followed, 
+                                    :dependent => :destroy
 
-	validates :name, 	:presence 			=> true, 
-					  	:length 				=> { :maximum => 50 }
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
 
-	validates :email, 	:presence			=> true,
-						:format 			=> { :with 	=> email_regex },
-						:uniqueness 		=> { :case_sensitive => false }
+  has_many :followers, :through => :reverse_relationships, 
+                                    :source => :follower
 
- 	validates :password, 	:presence			=> true,
- 							:confirmation		=> true,
- 							:length				=> { :within => 6..40 }
+  email_regex = /\A[\w.+\-]+@[a-zA-Z\-\d.]+\.[a-zA-Z]{1,6}\z/
 
- 	before_save :encrypt_password 
+  validates :name,  :presence       => true, 
+              :length         => { :maximum => 50 }
 
- 	def has_password?(submitted_password)
- 		self.encrypted_password == encrypt(submitted_password)
- 	end
- 	
- 	def feed
+  validates :email,   :presence     => true,
+            :format       => { :with  => email_regex },
+            :uniqueness     => { :case_sensitive => false }
+
+  validates :password,  :presence     => true,
+              :confirmation   => true,
+              :length       => { :within => 6..40 }
+
+  before_save :encrypt_password 
+
+  def has_password?(submitted_password)
+    self.encrypted_password == encrypt(submitted_password)
+  end
+  
+  def feed
     Micropost.where("user_id = ?", id)
- 	end
+  end
 
- 	class << self 
- 		def authenticate(email, submitted_password)
- 			user = User.find_by_email(email)
- 			(user && user.has_password?(submitted_password)) ? user : nil
- 		end
+  def following?(followed)
+    self.relationships.find_by_followed_id(followed)
+  end
 
- 		def authenticate_with_salt(id, cookie_salt)
- 			user = find_by_id(id)
- 			(user && user.salt == cookie_salt) ? user : nil
- 		end
- 	end
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
+
+  class << self 
+    def authenticate(email, submitted_password)
+      user = User.find_by_email(email)
+      (user && user.has_password?(submitted_password)) ? user : nil
+    end
+
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
+    end
+
+  end
 
 
- 	private 
+  private 
 
- 		def encrypt_password
- 			self.salt = make_salt if self.new_record?
- 			self.encrypted_password = encrypt( self.password )
- 		end
+    def encrypt_password
+      self.salt = make_salt if self.new_record?
+      self.encrypted_password = encrypt( self.password )
+    end
 
- 		def encrypt(string)
- 			secure_hash("#{self.salt}--#{string}")		# not the final implementation, need to encrypt
- 		end
+    def encrypt(string)
+      secure_hash("#{self.salt}--#{string}")    # not the final implementation, need to encrypt
+    end
 
- 		def secure_hash(string)
- 			Digest::SHA2.hexdigest(string)
- 		end
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 
- 		def make_salt
- 			secure_hash("#{Time.now.utc}--#{password}")
- 		end
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
 end
 
 # == Schema Information
